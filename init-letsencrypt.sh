28170747e83574150e69c734e7418fd3f14b39a3
ko2 <ko22009@yandex.ru>
2022-12-17 21:37:47 +0500

    update docker

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -2,7 +2,7 @@
 
 domains=(effect.report)
 email="effect.bitrix@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 data_path="./certbot"
 rsa_key_size=4096
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
+For start need start docker:
+`docker-compose up -d`
+
 ##ON LOCAL
-**If you run first time on local machine - need run this command**
 `sudo ./init-ssl.sh`
 
 ##ON PROD
-**If you are on a prod, run init-letsencrypt.sh**
 `sudo ./init-letsencrypt.sh`
