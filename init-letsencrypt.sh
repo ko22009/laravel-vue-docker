48a287db4778151c4ff1db7b9ba3d94b97947df1
ko2 <ko22009@yandex.ru>
2022-12-17 22:24:03 +0500

    fix nginx container

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -61,7 +61,7 @@ done
 
 echo "### Starting nginx ..."
 # Restarting for case if nginx container is already started
-docker-compose up -d nginx && docker-compose restart nginx
+docker-compose up -d frontend_nginx && docker-compose restart frontend_nginx
 
 # Select appropriate email arg
 case "$email" in
